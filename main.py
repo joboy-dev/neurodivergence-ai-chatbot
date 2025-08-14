4a9a6c08aecdc11b54540b97cc0cd7e1ab73ea33
Adegbehingbe Oluwakorede <oluwakoredeadegbehingbe@gmail.com>
2025-08-14 16:07:21 +0100

    fix: create log file on startup

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import os
 from app.run import load_pages
 from db.database import create_database, load_db
 from services.rag import RAGService
 
 
+os.makedirs('logs', exist_ok=True)
 create_database()
 
 load_db()
